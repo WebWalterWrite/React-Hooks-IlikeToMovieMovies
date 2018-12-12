3b4d7c6306a56c56c2fd725092f69bbc7682ded0
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-12-12 17:56:16 +0100

    type:style desc: ajout style au component Episode

---
+++ src/components/Episode/episode.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+
+export const Modal = styled.div`
+ position:absolute;
+ top:0;
+ left:0;
+ height:100vh;
+ width:100vw;
+ background-color:#000000;
+ color:#FFFFFF;
+ z-index:9999;
+`;
