581a74e5bb41abedb1f71a85a52fb5264159a18c
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-12-12 17:52:34 +0100

    type: functionnal desc: - ajout modal pour afficher épisode - ajout lazy et suspense

--- src/components/Season/Season.js
+++ src/components/Season/Season.js
@@ -1,49 +1,75 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect, lazy, Suspense } from "react";
 
+// Import fetch api
 import { getSeason } from "../../utils/api";
 
 import { Container, Overview, Episode, Img } from "./season.styled";
 
+// Import lazy component
+const LazyEpisode = lazy( ()=> import("../Episode/Episode"));
+
 const Season = props => {
 	const saison = props.match.params.id; // id saison
 
-    const [episode, setEpisode] = useState([]);
+	const [episode, setEpisode] = useState([]);
+	const [visible, setVisible] = useState(false);
+	const [data, setData] = useState([]);
 
 	useEffect(() => {
 		(async () => {
 			const result = await getSeason(saison);
-			console.log(result)
-            setEpisode(result);
-      
+			setEpisode(result);
 		})();
 	}, []);
 
-	const { episodes, overview } = episode;
 
+	const toggleModal = (saison, episode_number, overview, name) => {
+		
+		const data ={
+			saison:saison,
+			episode:episode_number,
+			desc:overview,
+			name:name
+		}
+		setData(data)
+		setVisible(true)
+		window.scrollTo({ 
+			top:0, 
+			behavior:"smooth"
+		})
+	}
+
+	const { episodes, overview } = episode;
+	
 	return (
 		<Fragment>
-			<h1>Saisons {saison }</h1>
+			{visible && 
+			<Suspense fallback={<div>Loading...</div>}>
+				<LazyEpisode  data={data} />
+			</Suspense>
+			}
+			<h1>Saisons {saison}</h1>
 			<Container>
 				<Overview>{overview}</Overview>
 
 				{episodes &&
-					episodes.map(({ name, still_path, episode_number }, k) => (
+					episodes.map(({ name, still_path, episode_number, overview }, k) => (
 						<Fragment key={k}>
-							
 							<Episode>
-							 <h1>Episode {episode_number}</h1>
-							
+								<h1>Episode {episode_number}</h1>
+
 								<Img>
 									<h2>{name}</h2>
 									<img
 										src={`https://image.tmdb.org/t/p/w500/${still_path}`}
 										alt="illustration épisode"
 									/>
-									<div><p>Découvrir</p></div>
+									<div>
+									<p onClick={()=>toggleModal(saison, episode_number, overview, name)}>Découvrir</p>
+									</div>
 								</Img>
-								
+
 							</Episode>
-						
 						</Fragment>
 					))}
 			</Container>
