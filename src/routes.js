f0ed5dd167884c1a81415ae56ae849bb9554e3da
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-12-13 17:47:23 +0100

    type: routing desc: creation route /got/saison/:saison/episode/:episode/:name

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,25 @@
-import Homepage from './components/Home/Homepage';
-import Seasons from './components/Seasons/Seasons';
-import Season from './components/Season/Season';
-const AllRoutes = [
-    { 
-        path:'/',
-        component:Homepage,
-    },
-    {
-        path:'/got/seasons',
-        component:Seasons,
- 
-    },
-    {
-        path:'/got/saison/:id/',
-        component:Season
+import Homepage from "./components/Home/Homepage";
+import Seasons from "./components/Seasons/Seasons";
+import Season from "./components/Season/Season";
+import Epd from "./components/Episode/Epd";
 
-    }
+const AllRoutes = [
+	{
+		path: "/",
+		component: Homepage
+	},
+	{
+		path: "/got/seasons",
+		component: Seasons
+	},
+	{
+		path: "/got/saison/:id/",
+		component: Season
+	},
+	{
+		path: "/got/saison/:saison/episode/:episode/:name",
+		component: Epd
+	}
 ];
 
 export default AllRoutes;
