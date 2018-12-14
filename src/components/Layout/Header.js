a3c0ffd5f2b96f323d9f262cc81303fa5c70fadf
Sammy el dueno <sammyeldueno@iMac-de-Sammy.local>
2018-12-14 20:54:40 +0100

    type:refactor desc: add link to homepage

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import {H1} from './layout.styled';
 const Header = () => (
-    <header>
-        <H1>Oh my GOT</H1>
-    </header>
+  <header>
+    <H1>
+      <Link to="/">Oh my GOT</Link>
+    </H1>
+  </header>
 );
 
 export default Header;
