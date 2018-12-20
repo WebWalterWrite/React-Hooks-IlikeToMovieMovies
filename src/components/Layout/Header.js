4f3e1881b1026c600469cf4dfeef7a03313f2834
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-12-20 18:19:22 +0100

    type: fonctionnel desc: ajout fonction back to top ( smooth non compatible navigateur mobile )

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import { Link } from 'react-router-dom';
 
-import {H1} from './layout.styled';
-const Header = () => (
-  <header>
-    <H1>
-      <Link to="/">OH MY GOT</Link>
-    </H1>
-  </header>
-);
+// import icon
+import { faArrowCircleUp } from '@fortawesome/free-solid-svg-icons';
+
+// import styles
+import {H1, Back} from './layout.styled';
+const Header = () => {
+  // State
+  const [pos, setPos ] = useState({top: 0, isHidden:true});
+
+  // Afficher icon haut de page
+  const BackToTop = () => {
+    window.scrollY > 1600
+    ? setPos({isHidden:false}) 
+    : setPos({isHidden:true})
+  }
+  // Retour Haut de page
+  const ClickToBack =(e)=> {
+      window.scrollTo({
+        top:0,
+        behavior:'smooth'
+      })  
+  }
+
+  // Ecoutér le scroll
+  window.onscroll = () => BackToTop();
+
+  return (
+    <Fragment>
+        {!pos.isHidden && <Back onClick={ClickToBack} icon={faArrowCircleUp} size="3x"/>}
+      <header>
+      <H1>
+        <Link to="/">OH MY GOT</Link>
+      </H1>
+      </header>
+    </Fragment>
+)};
 
 export default Header;
