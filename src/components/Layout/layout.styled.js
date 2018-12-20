5a0a658ce6bdebe0536048b0a232fb0a03a2dbea
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-12-20 18:20:09 +0100

    type:style desc: style back tot top

--- src/components/Layout/layout.styled.js
+++ src/components/Layout/layout.styled.js
@@ -1,6 +1,5 @@
-import styled from 'styled-components';
-
-
+import styled, {keyframes} from 'styled-components';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 export const H1 = styled.h1`
     color:white;
     font-size: 2em;
@@ -8,5 +7,25 @@ export const H1 = styled.h1`
     a{
         text-decoration:none;
         color:#FFFFFF;
+    }
+`;
+
+const Btn = keyframes`
+    from{right:-10%};
+    to{right:2%};
+`
+
+export const Back = styled(FontAwesomeIcon)`
+    position:fixed;
+    bottom:3%;
+    z-index:100000;
+    right:1%;
+    color:rgba(254, 204, 1, 0.7);
+    transition: all 1s ease-in-out;
+    animation: ${Btn} .5s linear;
+    transition:all .2s ease-in-out;
+    cursor:pointer;
+    :hover{
+        color:rgb(254, 204, 1);
     }
 `;
