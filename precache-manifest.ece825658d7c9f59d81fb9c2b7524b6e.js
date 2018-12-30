3d29b0f38d9712b6cd51cde651fe4dbc1a180eef
Sammy el dueno <sammyeldueno@iMac-de-Sammy.local>
2018-12-30 18:09:36 +0100

    Updates

---
+++ precache-manifest.ece825658d7c9f59d81fb9c2b7524b6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3996a4aca4f511a1b7ca40d5d55bbef",
+    "url": "/React-Hooks-IlikeToMovieMovies/got/homepage/static/media/got.a3996a4a.png"
+  },
+  {
+    "revision": "79d6bb2cdbec492268ba514878a4ef8e",
+    "url": "/React-Hooks-IlikeToMovieMovies/got/homepage/static/media/backgroundgot.79d6bb2c.jpg"
+  },
+  {
+    "revision": "860a5583214604e783e8",
+    "url": "/React-Hooks-IlikeToMovieMovies/got/homepage/static/js/runtime~main.860a5583.js"
+  },
+  {
+    "revision": "8d64bee298f349c6302f",
+    "url": "/React-Hooks-IlikeToMovieMovies/got/homepage/static/js/main.8d64bee2.chunk.js"
+  },
+  {
+    "revision": "81d1dc4975ebd0e920b9",
+    "url": "/React-Hooks-IlikeToMovieMovies/got/homepage/static/js/1.81d1dc49.chunk.js"
+  },
+  {
+    "revision": "53763fd103bae1127256cac96b8df8cd",
+    "url": "/React-Hooks-IlikeToMovieMovies/got/homepage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Hooks-IlikeToMovieMovies/precache-manifest.71b5db4f329dbd98f11b675242c2d8a8.js"
+  "/React-Hooks-IlikeToMovieMovies/got/homepage/precache-manifest.ece825658d7c9f59d81fb9c2b7524b6e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/React-Hooks-IlikeToMovieMovies/index.html", {
+workbox.routing.registerNavigationRoute("/React-Hooks-IlikeToMovieMovies/got/homepage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
