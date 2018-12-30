d403c45ae8f4272f035aa66bd8590bd3275f2e8b
Sammy el dueno <sammyeldueno@iMac-de-Sammy.local>
2018-12-30 18:04:37 +0100

    Updates

---
+++ precache-manifest.c668bcb861e6b2a60969af4bc7238154.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3996a4aca4f511a1b7ca40d5d55bbef",
+    "url": "://WebWalterWrite.github.io/React-Hooks-IlikeToMovieMovies/static/media/got.a3996a4a.png"
+  },
+  {
+    "revision": "79d6bb2cdbec492268ba514878a4ef8e",
+    "url": "://WebWalterWrite.github.io/React-Hooks-IlikeToMovieMovies/static/media/backgroundgot.79d6bb2c.jpg"
+  },
+  {
+    "revision": "49e9c817b1aa9efe37c6",
+    "url": "://WebWalterWrite.github.io/React-Hooks-IlikeToMovieMovies/static/js/runtime~main.49e9c817.js"
+  },
+  {
+    "revision": "c5b823972027eed65506",
+    "url": "://WebWalterWrite.github.io/React-Hooks-IlikeToMovieMovies/static/js/main.c5b82397.chunk.js"
+  },
+  {
+    "revision": "5ed7f0c8b2d7bf500a6b",
+    "url": "://WebWalterWrite.github.io/React-Hooks-IlikeToMovieMovies/static/js/1.5ed7f0c8.chunk.js"
+  },
+  {
+    "revision": "0cd533aba6431bd0b225a18877c7d5c7",
+    "url": "://WebWalterWrite.github.io/React-Hooks-IlikeToMovieMovies/index.html"
+  }
+];
