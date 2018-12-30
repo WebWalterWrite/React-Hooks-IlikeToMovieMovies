7d2be07d2b41331b624ffef36797aa95c9917d25
Sammy el dueno <sammyeldueno@iMac-de-Sammy.local>
2018-12-30 18:13:08 +0100

    Updates

---
+++ precache-manifest.fbf32fdc7f97bf5c8089335cd56bea23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3996a4aca4f511a1b7ca40d5d55bbef",
+    "url": "/got/homepage/static/media/got.a3996a4a.png"
+  },
+  {
+    "revision": "79d6bb2cdbec492268ba514878a4ef8e",
+    "url": "/got/homepage/static/media/backgroundgot.79d6bb2c.jpg"
+  },
+  {
+    "revision": "d84d6a9c2eaf9c107624",
+    "url": "/got/homepage/static/js/runtime~main.d84d6a9c.js"
+  },
+  {
+    "revision": "097f0c0b0e26175044ed",
+    "url": "/got/homepage/static/js/main.097f0c0b.chunk.js"
+  },
+  {
+    "revision": "45acb22095024c73a595",
+    "url": "/got/homepage/static/js/1.45acb220.chunk.js"
+  },
+  {
+    "revision": "04c7a1ce42b827d45975120f1f5a57df",
+    "url": "/got/homepage/index.html"
+  }
+];
